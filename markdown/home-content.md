3840ff646b3f7e6b084fdefc8c4c9e141a9665eb
Evan Williams <m3fawner@gmail.com>
2020-12-29 13:37:49 -0800

    chore: Move home to markdown

---
+++ markdown/home-content.md
@@ -0,0 +1,17 @@
+#### Welcome to FIcarious!
+
+<br/>
+
+##### What is this site?
+
+<br/>
+
+This site is really just a testing ground for technologies I am interested in studying and learning more about. For instance, this is a React Server Side rendered site using Chakra UI. Part of my job is working on interesting web problems, and the best way to come up with solutions is to play around outside of the project. This project is entirely centered around that, for now!
+
+<br/>
+
+##### What's here?
+
+<br/>
+
+My go to hobby has been finances for a very long time. As a result, it's the most "in reach" topic for my test projects. I'll be building out finance tools because it's what I know best and allows me to focus on the code problems and not the product problems. For now, I have a megabackdoor Roth page as well as a net RSU grant proceeds page because they are common questions I get at work. I'll probably add tools as I take time off (warning, probably very few updates). Check the menu for what's available!
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,20 @@
-import { Text, Heading } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+import getMarkdown from '../markdown/getMarkdown';
 import Head from '../src/components/Head';
+import MarkdownConverter from '../src/components/MarkdownConverter';
 
-const Home = () => (
+const Home = ({ content }) => (
   <>
     <Head title="Home" description="Welcome to FIcarious! This is a site dedicated to teaching about financial independence with helpful tools and articles about the subject." url="" />
-    <Heading>Welcome to FIcarious!</Heading>
-    <Heading as="h4" size="md" mt="4">What is this site?</Heading>
-    <Text mt="2">
-      &nbsp;&nbsp;This site is really just a testing ground for technologies I am interested
-      in studying and learning more about. For instance, this is a React Server Side
-      rendered site using Chakra UI.
-    </Text>
-    <Heading as="h4" size="md" mt="2">What&apos;s here?</Heading>
-    <Text>
-      &nbsp;&nbsp;Well, my go to hobby is finances...so here we are, building finance tools
-      because it&apos;s what I know best! I will probably add tools as I take days off...
-      so rarely! Right now, there is a net RSU grant proceeds calculator. Check the menu for info!
-    </Text>
+    <MarkdownConverter markdown={content} />
   </>
 );
+Home.propTypes = {
+  content: PropTypes.string.isRequired,
+};
+export const getStaticProps = async () => ({
+  props: {
+    content: await getMarkdown('home-content'),
+  },
+});
 export default Home;
