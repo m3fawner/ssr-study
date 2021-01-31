2e8a8ae236150514cbaa088d72faec17841dd30f
Evan Williams <m3fawner@gmail.com>
2021-01-31 08:11:22 -0800

    feat: Add GA

---
+++ pages/_document.jsx
@@ -0,0 +1,33 @@
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+export default class SSRDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.GA_ID}');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
