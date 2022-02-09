0aab42350c2cb395ffc69645b68740231f72c2cf
Evan Williams <evanwi@zillowgroup.com>
2022-02-09 11:48:43 -0800

    task(401k): Add content, replace 'coming eventually' placeholder

--- pages/401k/index.jsx
+++ pages/401k/index.jsx
@@ -1,1 +1,21 @@
-export default () => <div>Coming....eventually?</div>;
+import PropTypes from 'prop-types';
+import Head from '../../src/components/Head';
+import MarkdownConverter from '../../src/components/MarkdownConverter';
+import getMarkdown from '../../markdown/getMarkdown';
+
+const FourOhOneK = ({ fourOhOneK }) => (
+  <>
+    <Head title="401k" description="An introduction to 401(k)s, a tax-advantaged account." url="401k" />
+    <MarkdownConverter pt="5" markdown={fourOhOneK} />
+  </>
+);
+FourOhOneK.propTypes = {
+  fourOhOneK: PropTypes.string.isRequired,
+};
+
+export const getStaticProps = async () => ({
+  props: {
+    fourOhOneK: await getMarkdown('401k'),
+  },
+});
+export default FourOhOneK;
