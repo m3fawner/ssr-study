90589fa8cf8cf823d2972ee596542bbb2c1e67e1
Evan Williams <evanwi@zillowgroup.com>
2022-02-11 09:04:40 -0800

    feat(hsa)

---
+++ pages/tax-advantaged-accounts/hsa.jsx
@@ -0,0 +1,5 @@
+import createMarkdownPage from '../../src/util/createMarkdownPage';
+
+const { Component, _getStaticProps } = createMarkdownPage('hsa', 'Health Savings Account', 'Health savings accounts are a great method of saving for medical expenses, providing pre-tax funds to reimburse medical expenses incurred while on an HDHP. This article also covers how it is a potentially great retirement vehicle, as well!', 'tax-advantaged-accounts/hsa');
+export const getStaticProps = _getStaticProps;
+export default Component;
