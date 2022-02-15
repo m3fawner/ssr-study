8df1cc119703ca42bfb27f91ed42d04417aaaa13
Evan Williams <evanwi@zillowgroup.com>
2022-02-15 08:06:32 -0800

    feat(pwa)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
-module.exports = withBundleAnalyzer({
+module.exports = withPWA(withBundleAnalyzer({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
   pageExtensions: ['jsx', 'js'],
   i18n: {
     locales: ['en-US'],
@@ -20,4 +26,4 @@ module.exports = withBundleAnalyzer({
 
     return config;
   },
-});
+}));
--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -14,8 +14,12 @@ const Head = ({
       <meta name="og:title" content={finalTitle} />
       <meta name="og:description" content={description} />
       <meta name="og:url" content={`${process.env.NEXT_PUBLIC_BASE_URL}/${url}`} />
-      <meta name="og:image" content="/favicon-32x32.png" />
-      <link rel="icon" href="/favicon-32x32.png" />
+      <meta name="og:image" content="/assets/icons/icon-48x48.png" />
+      <link rel="icon" href="/assets/icons/icon-48x48.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#C1E7EB" />
+      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#23656C" />
+      <link rel="apple-touch-icon" href="/assets/icons/icon-192x192.png" />
     </NextHead>
   );
 };
