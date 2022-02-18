c5e0d01affa3b5d5efb0aa5ea680ebb58d20377f
Evan Williams <evanwi@zillowgroup.com>
2022-02-18 07:47:29 -0800

    chore(pwa): Disable pwa on development

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ module.exports = withPWA(withBundleAnalyzer({
   pwa: {
     dest: 'public',
     runtimeCaching,
+    disable: process.env.NODE_ENV === 'development',
   },
   pageExtensions: ['jsx', 'js'],
   i18n: {
