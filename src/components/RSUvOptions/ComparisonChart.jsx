5b3c82e9732867f006ad788ecf3904789d025fab
Evan Williams <m3fawner@gmail.com>
2022-06-28 08:17:21 -0700

    feat(recharts): Remove react-vis due to react 18 support, implement recharts

--- src/components/RSUvOptions/ComparisonChart.jsx
+++ src/components/RSUvOptions/ComparisonChart.jsx
@@ -1,41 +1,37 @@
 import { Flex, useToken, useBreakpointValue } from '@chakra-ui/react';
-import { useMemo, useState } from 'react';
-import 'react-vis/dist/style.css';
 import {
-  LineSeries, XAxis, XYPlot, YAxis, DiscreteColorLegend, Crosshair,
-} from 'react-vis';
+  LineChart, XAxis, YAxis, Tooltip, Legend, Line, CartesianGrid,
+} from 'recharts';
 import { dataRowsPropTypes } from './propTypes';
 import { getDollar } from '../../util/formatters';
 
 const ComparisonChart = ({ dataRows }) => {
-  const [crosshairValues, setCrosshairValues] = useState();
-  const { optionsLine, rsusLine } = useMemo(() => dataRows.reduce(({ optionsLine: opts, rsusLine: rsus }, { rowPrice, rsuValue, optionValue }) => ({
-    optionsLine: [...opts, { x: rowPrice, y: optionValue }],
-    rsusLine: [...rsus, { x: rowPrice, y: rsuValue }],
-  }), {
-    optionsLine: [],
-    rsusLine: [],
-  }), [dataRows]);
   const [optionsLineColor, rsusLineColor] = useToken('colors', ['red.600', 'green.900']);
   const chartHeight = useBreakpointValue({ base: 180, md: 320 }) ?? 180;
   const chartWidth = useBreakpointValue({ base: 320, md: 540 }) ?? 320;
   return (
     <Flex justifyContent="center" flexDir={['column',, 'row']}>
-      <XYPlot width={chartWidth} height={chartHeight} animation onMouseLeave={() => setCrosshairValues(null)}>
-        <Crosshair values={crosshairValues} itemsFormat={([{ y: options }, { y: rsus }]) => [{ title: 'Option value', value: options }, { title: 'RSUs value', value: rsus }]} />
-        <XAxis title="Grant price" tickFormat={getDollar} />
-        <YAxis title="Share price" tickFormat={getDollar} />
-        <LineSeries
-          color={optionsLineColor}
-          data={optionsLine}
-          onNearestX={(_, { index }) => setCrosshairValues([optionsLine[index], rsusLine[index]])}
+      <LineChart width={chartWidth} height={chartHeight} data={dataRows}>
+        <Tooltip labelFormatter={(grantPrice) => `Grant price: $${grantPrice}`} formatter={getDollar} />
+        <Legend />
+        <XAxis dataKey="rowPrice" label={{ value: 'Grant price', dy: 12, position: 'insideBottomRight' }} tickFormatter={getDollar} />
+        <YAxis label={{ value: 'Share price', position: 'insideLeft', angle: -90 }} tickFormatter={getDollar} />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Line
+          type="monotone"
+          isAnimationActive={false}
+          stroke={optionsLineColor}
+          dataKey="optionValue"
+          name="Options value"
         />
-        <LineSeries
-          color={rsusLineColor}
-          data={rsusLine}
+        <Line
+          type="monotone"
+          isAnimationActive={false}
+          stroke={rsusLineColor}
+          dataKey="rsuValue"
+          name="RSUs value"
         />
-      </XYPlot>
-      <DiscreteColorLegend style={{ overflowY: 'hidden' }} items={[{ title: 'Options value', color: optionsLineColor }, { title: 'RSUs value', color: rsusLineColor }]} orientation="horizontal" width={200} height={45} />
+      </LineChart>
     </Flex>
   );
 };
